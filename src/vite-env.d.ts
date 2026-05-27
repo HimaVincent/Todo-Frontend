5823723f26e3d9a142176e2391496fff336bce80
HimaVincent <himavincent@outlook.com>
2026-05-27 12:35:57 +1000

    Prepare frontend for Railway deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,11 @@ declare module "*.svg?react" {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
